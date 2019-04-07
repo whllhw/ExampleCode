8afab3f6cac3da732b3fbfa56beb263487211209
lhw <990230292@qq.com>
2019-04-08 00:54:54 +0800

    add 动态规划 && 剑指offer二进制1的个数

---
+++ src/main/java/alg/template/Base.java
@@ -0,0 +1,20 @@
+package alg.template;
+
+import java.io.PrintStream;
+import java.util.Arrays;
+
+public class Base {
+    protected static PrintStream out = System.out;
+
+    protected static void println(int[] t) {
+        out.println(Arrays.toString(t));
+    }
+
+    protected static void println(int[][] t) {
+        out.println("[");
+        for (int i = 0; i < t.length; i++) {
+            println(t[i]);
+        }
+        out.println("]");
+    }
+}
