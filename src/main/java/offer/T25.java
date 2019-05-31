1c795bd85a17c6b9a6d97f3dd2e60df12b34649a
lhw <990230292@qq.com>
2019-06-01 00:10:25 +0800

    add 剑指offer第15-28题

---
+++ src/main/java/offer/T25.java
@@ -0,0 +1,68 @@
+package offer;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.LinkedList;
+
+public class T25 {
+    private LinkedList<TreeNode> stack = new LinkedList<>();
+
+    private ArrayList<ArrayList<Integer>> ans = new ArrayList<>();
+    private int sum = 0;
+
+    private int target;
+
+    public static void main(String[] args) {
+        LinkedList<Integer> stack = new LinkedList<>();
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+        Iterator<Integer> iter = stack.descendingIterator();
+        while (iter.hasNext()) {
+            System.out.println(iter.next());
+        }
+    }
+
+    /**
+     * 二叉树中和为某一值
+     *
+     * @param root   root
+     * @param target 目标值
+     * @return 所有符合的路径
+     */
+    public ArrayList<ArrayList<Integer>> FindPath(TreeNode root, int target) {
+        // 思路：画出访问二叉树的访问过程
+        // 找规律得知，用前序遍历可以实现，用栈记录访问的路径，当访问节点时压入，
+        // 访问完左右子树时退栈，同时sum减去val值
+        if (root == null)
+            return ans;
+        this.target = target;
+        helper(root);
+        return ans;
+    }
+
+    private void helper(TreeNode root) {
+        // 加入路径
+        stack.push(root);
+        sum += root.val;
+        // 当前sum等于目标值，且节点为叶子节点则把路径放入结果中
+        if (sum == target && root.left == null && root.right == null) {
+            ArrayList<Integer> path = new ArrayList<>();
+            // 使用descendingIterator，才是正确的路径，否则是倒序的
+            Iterator<TreeNode> iter = stack.descendingIterator();
+            while (iter.hasNext()) {
+                path.add(iter.next().val);
+            }
+            ans.add(path);
+        }
+        if (root.left != null) {
+            helper(root.left);
+        }
+        if (root.right != null) {
+            helper(root.right);
+        }
+        // 退栈时同时减去val
+        stack.pop();
+        sum -= root.val;
+    }
+}
---
+++ src/main/java/offer/T27.java
@@ -0,0 +1,131 @@
+package offer;
+
+import java.util.LinkedList;
+
+public class T27 {
+    private TreeNode leftLast;
+
+    public static void main(String[] args) {
+//        TreeNode root = new TreeNode(10, new TreeNode(6, new TreeNode(4), new TreeNode(8)
+//        ), new TreeNode(14, new TreeNode(12), new TreeNode(16))
+//        );
+        TreeNode root = new TreeNode(1, new TreeNode(0), new TreeNode(2));
+        T27 t = new T27();
+        TreeNode head = t.Convert(root);
+        System.out.println(head);
+    }
+
+    /**
+     * 二叉搜索树与双向链表
+     *
+     * @param root 搜索树
+     * @return
+     */
+    public TreeNode Convert(TreeNode root) {
+        // 未改进版递归
+        // 思路:递归结束条件，本级要返回的值，本级该做的事情
+        // 定义返回链表头的函数
+        // 同样使用中序遍历来获取有序链表
+        // 在进行赋值时遍历到链表末尾才进行连接
+        // 处理玩当前节点再对右子树进行处理
+
+        // 注意判空
+        if (root == null)
+            return null;
+        if (root.left == null && root.right == null)
+            return root;
+        // 获取左子树的链表头
+        TreeNode left = Convert(root.left);
+        TreeNode p = left;
+        // 找到链表末尾
+        while (p != null && p.right != null)
+            p = p.right;
+        // 进行连接
+        if (left != null) {
+            root.left = p;
+            p.right = root;
+        }
+        // 遍历右子树
+        TreeNode right = Convert(root.right);
+        // 连接
+        if (right != null) {
+            right.left = root;
+            root.right = right;
+        }
+        // 返回链表头
+        return left == null ? root : left;
+    }
+
+    /**
+     * 递归改进版
+     *
+     * @param root 节点
+     * @return 链表头
+     */
+    public TreeNode convert0(TreeNode root) {
+        // 添加了一个leftLast记录上次的链表末尾
+        // 共两种情况时当前节点是末尾
+        if (root == null)
+            return null;
+        if (root.left == null && root.right == null) {
+            // 1. 叶子节点
+            leftLast = root;
+            return root;
+        }
+        TreeNode left = convert0(root.left);
+        if (left != null) {
+            leftLast.right = root;
+            root.left = leftLast;
+        }
+        // 2.添加进了左子树，当前节点就是末尾了
+        leftLast = root;
+        TreeNode right = convert0(root.right);
+        if (right != null) {
+            root.right = right;
+            right.left = root;
+        }
+        return left == null ? root : left;
+    }
+
+    /**
+     * 非递归版本
+     *
+     * @param root
+     * @return
+     */
+    public TreeNode convert1(TreeNode root) {
+        // 思路：
+        // 同中序遍历，不过需要flag记录链表头
+        // 以及pre节点，用于连接
+        if (root == null)
+            return null;
+        LinkedList<TreeNode> stack = new LinkedList<>();
+        TreeNode p = root;
+        TreeNode pre = null;
+        boolean first = true;
+        while (p != null || stack.size() != 0) {
+            // 一直往左走
+            while (p != null) {
+                stack.push(p);
+                p = p.left;
+            }
+            p = stack.pop();
+            // 链表头则保存下来
+            if (first) {
+                root = p;
+                pre = root;
+                first = false;
+            } else {
+                // 连接
+                // 并更新pre
+                pre.right = p;
+                p.left = pre;
+                pre = p;
+            }
+            // 中序标准代码
+            p = p.right;
+        }
+        return root;
+    }
+
+}
